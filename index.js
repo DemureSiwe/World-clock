26d0b647cefd02077dfb4fda656291d3abdc57d7
DemureBusi <letsetse.b@gmail.com>
2025-04-08 13:31:12 +0200

    added js content

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert("Hello");
+function updateTime() {
+  let southAfricaElement = document.querySelector("#south-africa");
+  let southAfricaDateElement = southAfricaElement.querySelector(".date");
+  let southAfricaTimeElement = southAfricaElement.querySelector(".time");
+  let southAfricaTime = moment().tz("Africa/Johannesburg");
+
+  southAfricaDateElement.innerHTML = southAfricaTime.format("MMMM Do YYYY");
+  southAfricaTimeElement.innerHTML = southAfricaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let kenyaElement = document.querySelector("#kenya");
+  let kenyaDateElement = kenyaElement.querySelector(".date");
+  let kenyaTimeElement = kenyaElement.querySelector(".time");
+  let kenyaTime = moment().tz("Africa/Nairobi");
+
+  kenyaDateElement.innerHTML = kenyaTime.format("MMMM Do YYYY");
+  kenyaTimeElement.innerHTML = kenyaTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+setInterval(updateTime, 1000);
